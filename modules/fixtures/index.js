2825e99c1426273f4aa930070f66d683509df937
Mike Reich <mjreich@gmail.com>
2016-04-18 06:57:57 -0700

    Pushed up changes for templater api changes

--- modules/fixtures/index.js
+++ modules/fixtures/index.js
@@ -1,7 +1,7 @@
 /* 
 * @Author: Mike Reich
 * @Date:   2016-02-04 16:35:16
-* @Last Modified 2016-02-04
+* @Last Modified 2016-04-18
 */
 
 'use strict';
@@ -11,7 +11,7 @@ import Basic from './model'
 export default (app) => {
   app.get('storage').model(Basic)
   
-  app.get('router').route('GET', '/fixtures/name', (req, res) => {
+  app.get('router').route('/fixtures/name', (req, res) => {
     app.get('storage').getModel('basic').then((model) => {
       model.find({}).then((insts) => {
         let content = insts[0].name
--- modules/templater/index.js
+++ modules/templater/index.js
@@ -1,16 +1,16 @@
 /* 
 * @Author: Mike Reich
 * @Date:   2016-02-04 16:35:16
-* @Last Modified 2016-02-04
+* @Last Modified 2016-04-18
 */
 
 'use strict';
 
 export default (app) => {
-  app.get('templater').replace('template','page', 'ejs', __dirname+"/views/page.ejs")
-  app.get('templater').templateFile(__dirname+"/views/inner.ejs")
-  app.get('templater').templateFile(__dirname+"/views/render-in.ejs")
+  app.get('templater').template(__dirname+"/views/page.ejs")
+  app.get('templater').template(__dirname+"/views/inner.ejs")
+  app.get('templater').template(__dirname+"/views/render-in.ejs", "page")
   app.get('router').route('GET', '/templater/render-in', (req, res) => {
-    app.get('templater').render('render-in', 'page', {title: "Templater Page", opts: app.config}).then(res.send.bind(res));
+    app.get('templater').render('render-in', {title: "Templater Page", opts: app.config}).then(res.send.bind(res));
   })
 }
